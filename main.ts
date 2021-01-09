c10837211fcf61e6177701df51f21d09d5702f24
Lionkk <Lion_yan@outlook.com>
2021-01-09 15:33:44 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -10,6 +10,50 @@ namespace Drones {
         //% block="Landing"
         Landing = 0x02
     }
+    export enum Directionoptions{
+        //% block="Up" 
+        Up = 0x10,
+        //% block="Down"
+        Down = 0x11,
+        //% block="Forward" 
+        Forward = 0x12,
+        //% block="Backward"
+        Backward = 0x13,
+        //% block="Left" 
+        Left = 0x14,
+        //% block="Right"
+        Right = 0x15
+    }
+    export enum Angleoptions{
+        //% block="Left" 
+        Take_off = 0x16,
+        //% block="Right"
+        Landing = 0x17
+    }
+    export enum Rolloptions{
+        //% block="Roll forward" 
+        Roll_forward = 0x20,
+        //% block="Roll back"
+        Roll_back = 0x21,
+        //% block="Roll left" 
+        Roll_left = 0x22,
+        //% block="Roll right"
+        Roll_right = 0x23
+    }
+    function Dronesback():boolean{
+        basic.pause(300)
+        rxBuff = serial.readBuffer(3)
+        if(rxBuff[0] == 0x01 && rxBuff[1] == 0x01){
+            return true
+        }
+        else if(rxBuff[0] == 0x01 && rxBuff[1] == 0x02){
+            //执行错误
+        }
+        else{
+            return false
+        }
+        return false
+    }
     /**
     * TODO: Waiting for module initialize.
     */
@@ -17,20 +61,37 @@ namespace Drones {
     export function initModule():void{
         serial.redirect(SerialPin.P1, SerialPin.P2, 115200)
     }
-    /**
-     * TODO: Loop songs in folders
-     * @param folderNum Specify a floder , eg: 0
-     */
+
     //% blockId=fans block="Basic action %basicstate"
     export function Basic_action(basicstate: Basicoptions): void {
         let txBuff = pins.createBuffer(4)
+        let rxBuff = pins.createBuffer(3)
         txBuff[0] = 0xEF
         txBuff[1] = 0
         txBuff[2] = 0x01
         txBuff[3] = basicstate
         serial.writeBuffer(txBuff)
+        while(!Dronesback());
     }
-
+    //% blockId=fans block="Move action %basicstate by %distance"
+    export function Move_action(basicstate: Basicoptions,distance:number): void {
+        let txBuff = pins.createBuffer(6)
+        let rxBuff = pins.createBuffer(3)
+        txBuff[0] = 0xEF
+        txBuff[1] = 2
+        txBuff[2] = 0x01
+        txBuff[3] = basicstate
+        if(distance > 255){
+            txBuff[4] = 255
+            txBuff[5] = distance - 255
+        }
+        else{
+            txBuff[4] = distance
+            txBuff[5] = 0
+        }
+        serial.writeBuffer(txBuff)
+        while(!Dronesback());
+    }
     
 
     
